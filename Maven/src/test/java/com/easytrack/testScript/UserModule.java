3f34810ca63a3b2f6439dfdce7e791f893744693
Praveen M Ghali <Praveen M Ghali@LAPTOP-RL7JVSAU>
2025-03-12 20:00:28 +0530

    Selenium code added

--- Maven/src/test/java/com/easytrack/testScript/UserModule.java
+++ Maven/src/test/java/com/easytrack/testScript/UserModule.java
@@ -1,11 +1,20 @@
 package com.easytrack.testScript;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class UserModule {
 	@Test
 	public void name() {
-		Reporter.log("User module created successfully",true);
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.get("https://www.google.com/");
+	System.out.println(driver.getTitle());
+	 
 	}
 }
